db35a3365b0a455ed970677395c9690f308caa71
Thompson <cthompso@revenue.ie>
2018-11-26 11:13:01 +0000

    Added main functionality

--- src/main/java/ie/sesh/Controllers/Account/RegisterController.java
+++ src/main/java/ie/sesh/Controllers/Account/RegisterController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.env.Environment;
 
 @RestController
 public class RegisterController {
@@ -17,23 +18,34 @@ public class RegisterController {
     @Autowired
     RegisterDAO registerDAO;
 
+    @Autowired
+    Environment env;
+
     @PostMapping("/register/user")
     @ResponseBody
     public String registerUser(@RequestBody String user_data) throws Exception{
 
         final JSONObject obj = new JSONObject(user_data);
+        String username;
+        String password;
+        String email;
+        String name;
 
         log.info("Submitted data: "+user_data);
 
-        //String username = Authentication.encrypt(obj.getJSONArray("username").get(0).toString());
-        //String password = Authentication.hashPassword(obj.getJSONArray("password").get(0).toString());
-        //String email = Authentication.encrypt(obj.getJSONArray("email").get(0).toString());
-        //String name = Authentication.encrypt(obj.getJSONArray("name").get(0).toString());
+        if(env.getProperty("enable.encryption")) {
 
-        String username = obj.getJSONArray("username").get(0).toString();
-        String password = obj.getJSONArray("password").get(0).toString();
-        String email = obj.getJSONArray("email").get(0).toString();
-        String name = obj.getJSONArray("name").get(0).toString();
+             username = Authentication.encrypt(obj.getJSONArray("username").get(0).toString());
+             password = Authentication.hashPassword(obj.getJSONArray("password").get(0).toString());
+             email = Authentication.encrypt(obj.getJSONArray("email").get(0).toString());
+             name = Authentication.encrypt(obj.getJSONArray("name").get(0).toString());
+        }
+        else {
+             username = obj.getJSONArray("username").get(0).toString();
+             password = obj.getJSONArray("password").get(0).toString();
+             email = obj.getJSONArray("email").get(0).toString();
+             name = obj.getJSONArray("name").get(0).toString();
+        }
 
         log.info("NAME: "+name+"  HAS BEEN ADDED");
         if(registerDAO.registerUser(name,username,email,password)){
